13051ff8c0622ffc829ff18c1046e46b335725e8
Yasuyuki Kamata <yasuyuki@unity3d.com>
2020-06-29 04:18:14 +0900

    Add Custom Inspector for UnityAdsHelper

---
+++ Assets/UnityAdsHelper/Editor/UnityAdsHelperInspector.cs
@@ -0,0 +1,63 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityAdsHelper.Editor
+{
+    [CustomEditor(typeof(UnityAdsHelper))]
+    public class UnityAdsHelperInspector : UnityEditor.Editor
+    {
+	    private const string OperateDashboardUrl = "https://dashboard.unity3d.com";
+        private const string UnityAdsKnowledgeBaseUrl = "https://unityads.unity3d.com/help/index";
+        private const string UnityAdsForumUrl = "https://forum.unity.com/forums/unity-ads.67/";
+        
+        private void OnEnable()
+        {
+        }
+
+        public override void OnInspectorGUI()
+        {
+	        var helper = target as UnityAdsHelper;
+	        if (helper == null) return;
+	        
+	        EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Dashboard")) Help.BrowseURL(OperateDashboardUrl);
+            if (GUILayout.Button("Docs")) Help.BrowseURL(UnityAdsKnowledgeBaseUrl);
+            if (GUILayout.Button("Forum")) Help.BrowseURL(UnityAdsForumUrl);
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Game ID", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+
+            helper.GameIdAppleAppStore = EditorGUILayout.TextField("Apple AppStore", helper.GameIdAppleAppStore);
+            helper.GameIdGooglePlay = EditorGUILayout.TextField("GooglePlay", helper.GameIdGooglePlay);
+            
+            EditorGUILayout.Space();
+            EditorGUI.indentLevel--;
+
+            helper.UseAnotherGameIdForDevelopment = EditorGUILayout.Toggle("Use Another Game ID for Development", helper.UseAnotherGameIdForDevelopment);
+            
+            if (helper.UseAnotherGameIdForDevelopment)
+            {
+	            EditorGUILayout.LabelField("Game ID (Development)", EditorStyles.boldLabel);
+	            EditorGUI.indentLevel++;
+	            helper.GameIdAppleAppStoreForDevelopment = EditorGUILayout.TextField("Apple AppStore (Development)", helper.GameIdAppleAppStoreForDevelopment);
+	            helper.GameIdGooglePlayForDevelopment = EditorGUILayout.TextField("GooglePlay (Development)", helper.GameIdGooglePlayForDevelopment);
+	            EditorGUI.indentLevel--;
+            }
+            
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Options", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+            helper.InitializeOnStart = EditorGUILayout.Toggle("Initialize on Start", helper.InitializeOnStart);
+            helper.EnableTestMode = EditorGUILayout.Toggle("Enable Test Mode", helper.EnableTestMode);
+            EditorGUI.indentLevel--;
+            
+            EditorGUILayout.Space();
+            
+            //TODO: ここにリスナースクリプトを追加するボタン
+        }
+    }
+}
--- Assets/UnityAdsHelper/UnityAdsHelper.cs
+++ Assets/UnityAdsHelper/UnityAdsHelper.cs
@@ -1,23 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.Advertisements;
-using UnityEngine.Events;
-using UnityEngine.Monetization;
 
 namespace UnityAdsHelper
 {
 	public class UnityAdsHelper : MonoBehaviour
 	{
 		#pragma warning disable 0414
-		[SerializeField] private string gameIdAppleAppStore = "";
-		[SerializeField] private string gameIdGooglePlay = "";
-		[SerializeField] private bool useAnotherGameIdForTest = false;
-		[SerializeField] private string gameIdAppleAppStoreForTest = "";
-		[SerializeField] private string gameIdGooglePlayForTest = "";
+		[SerializeField] private string gameIdAppleAppStore = "3219868";
+		[SerializeField] private string gameIdGooglePlay = "3219869";
+		[SerializeField] private bool useAnotherGameIdForDevelopment = false;
+		[SerializeField] private string gameIdAppleAppStoreForDevelopment = "3219868";
+		[SerializeField] private string gameIdGooglePlayForDevelopment = "3219869";
 		[SerializeField] private bool initializeOnStart = true;
 		[SerializeField] private bool enableTestMode = false;
 		#pragma warning restore 0414
 		
-		private string _gameId = "";
+		private string _gameId;
 		private UnityAdsListener _listener;
 
 		private void Start()
@@ -32,10 +30,18 @@ namespace UnityAdsHelper
 		{
 			if (!Advertisement.isSupported || Advertisement.isInitialized) return;
 #if UNITY_IOS
-			_gameId = useAnotherGameIdForTest ? gameIdAppleAppStoreForTest : gameIdAppleAppStore;
+			_gameId = useAnotherGameIdForDevelopment && Debug.isDebugBuild ? gameIdAppleAppStoreForDevelopment : gameIdAppleAppStore;
 #elif UNITY_ANDROID
-			_gameId = useAnotherGameIdForTest ? gameIdGooglePlayForTest : gameIdGooglePlay; 
+			_gameId = useAnotherGameIdForDevelopment && Debug.isDebugBuild ? gameIdGooglePlayForDevelopment : gameIdGooglePlay;
+#else
+			Debug.Log("else");
 #endif
+			if (string.IsNullOrEmpty(_gameId))
+			{
+				Debug.LogWarning($"Unity Ads game id is empty. Please input your game id.");
+				return;
+			}
+
 			Advertisement.Initialize(_gameId, enableTestMode);
 
 			_listener = GetComponent<UnityAdsListener>();
@@ -44,16 +50,58 @@ namespace UnityAdsHelper
 				Advertisement.AddListener(_listener);
 			}
 		}
-
+		
 		public void ShowVideoAds(string placementId)
 		{
 			if (!Advertisement.IsReady(placementId)) return;
 			Advertisement.Show(placementId);
 		}
-		
-		void OnDestroy()
+
+		private void OnDestroy()
 		{
 			Advertisement.RemoveListener(_listener);
+		}
+		
+		public string GameIdGooglePlay
+		{
+			get => gameIdGooglePlay;
+			set => gameIdGooglePlay = value;
+		}
+
+		public string GameIdAppleAppStore
+		{
+			get => gameIdAppleAppStore;
+			set => gameIdAppleAppStore = value;
+		}
+
+		public bool UseAnotherGameIdForDevelopment
+		{
+			get => useAnotherGameIdForDevelopment;
+			set => useAnotherGameIdForDevelopment = value;
+		}
+
+		public string GameIdAppleAppStoreForDevelopment
+		{
+			get => gameIdAppleAppStoreForDevelopment;
+			set => gameIdAppleAppStoreForDevelopment = value;
+		}
+
+		public string GameIdGooglePlayForDevelopment
+		{
+			get => gameIdGooglePlayForDevelopment;
+			set => gameIdGooglePlayForDevelopment = value;
+		}
+
+		public bool InitializeOnStart
+		{
+			get => initializeOnStart;
+			set => initializeOnStart = value;
+		}
+
+		public bool EnableTestMode
+		{
+			get => enableTestMode;
+			set => enableTestMode = value;
 		}
 	}
 }
